87907581ffe9c01ef46e30c190edfec2f7285a1e
Ralph Renzo Pitogo <ralphrenzo96@gmail.com>
2022-11-21 19:13:22 +0800

    Added events and logics.

--- SalesTax/ViewController.swift
+++ SalesTax/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var beforeTaxPrice:Float = 0;
+    var salesTaxtRate:Float = 0;
+    
+    @IBOutlet weak var afterTaxPriceTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func BeforeTextChanged(_ sender: UITextField) {
+        if let value = Float(sender.text!)
+        {
+            beforeTaxPrice = value
+        }
+        else
+        {
+            beforeTaxPrice = 0;
+        }
+    }
+    
+    @IBAction func salesTaxRateChanged(_ sender: UITextField) {
+        if let value = Float(sender.text!)
+        {
+            salesTaxtRate = value
+        }
+        else
+        {
+            salesTaxtRate = 0;
+        }
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        
+        let salesTax = beforeTaxPrice * salesTaxtRate / 100
+        let afterTaxPrice = beforeTaxPrice + salesTax
+        
+        afterTaxPriceTextField.text = String(afterTaxPrice)
+    }
+    
 }
 
